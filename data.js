269e8c06eeba5467054092b20793035965a1cd41
Germain Rullier <grullier@free.fr>
2023-02-23 10:52:04 +0000

    Codes crimes

--- data.js
+++ data.js
@@ -2,482 +2,482 @@ const external_data = {
   "deps": [
     {
       "departement": "01",
-      "taux_criminalite": 3.75,
+      "taux_criminalite": 0.00091,
       "gagnant": "LE PEN"
     },
     {
       "departement": "02",
-      "taux_criminalite": 4.28,
+      "taux_criminalite": 0.00146,
       "gagnant": "LE PEN"
     },
     {
       "departement": "03",
-      "taux_criminalite": 3.86,
+      "taux_criminalite": 0.00144,
       "gagnant": "MACRON"
     },
     {
       "departement": "04",
-      "taux_criminalite": 4.9,
+      "taux_criminalite": 0.00119,
       "gagnant": "LE PEN"
     },
     {
       "departement": "05",
-      "taux_criminalite": 4.54,
+      "taux_criminalite": 0.0,
       "gagnant": "MACRON"
     },
     {
       "departement": "06",
-      "taux_criminalite": 6.46,
+      "taux_criminalite": 0.00082,
       "gagnant": "LE PEN"
     },
     {
       "departement": "07",
-      "taux_criminalite": 3.52,
+      "taux_criminalite": 0.0006,
       "gagnant": "LE PEN"
     },
     {
       "departement": "08",
-      "taux_criminalite": 4.07,
+      "taux_criminalite": 0.0,
       "gagnant": "LE PEN"
     },
     {
       "departement": "09",
-      "taux_criminalite": 4.16,
+      "taux_criminalite": 0.0,
       "gagnant": "M\u00c9LENCHON"
     },
     {
       "departement": "10",
-      "taux_criminalite": 4.9,
+      "taux_criminalite": 0.00158,
       "gagnant": "LE PEN"
     },
     {
       "departement": "11",
-      "taux_criminalite": 4.82,
+      "taux_criminalite": 0.00185,
       "gagnant": "LE PEN"
     },
     {
       "departement": "12",
-      "taux_criminalite": 2.87,
+      "taux_criminalite": 0.00035,
       "gagnant": "MACRON"
     },
     {
       "departement": "13",
-      "taux_criminalite": 7.35,
+      "taux_criminalite": 0.00205,
       "gagnant": "LE PEN"
     },
     {
       "departement": "14",
-      "taux_criminalite": 4.12,
+      "taux_criminalite": 0.00056,
       "gagnant": "MACRON"
     },
     {
       "departement": "15",
-      "taux_criminalite": 2.78,
+      "taux_criminalite": 0.0,
       "gagnant": "MACRON"
     },
     {
       "departement": "16",
-      "taux_criminalite": 4.28,
+      "taux_criminalite": 0.00083,
       "gagnant": "MACRON"
     },
     {
       "departement": "17",
-      "taux_criminalite": 4.74,
+      "taux_criminalite": 0.00076,
       "gagnant": "MACRON"
     },
     {
       "departement": "18",
-      "taux_criminalite": 4.19,
+      "taux_criminalite": 0.00032,
       "gagnant": "LE PEN"
     },
     {
       "departement": "19",
-      "taux_criminalite": 3.33,
+      "taux_criminalite": 0.0,
       "gagnant": "MACRON"
     },
     {
       "departement": "2A",
-      "taux_criminalite": 4.36,
+      "taux_criminalite": 0.00125,
       "gagnant": "LE PEN"
     },
     {
       "departement": "2B",
-      "taux_criminalite": 3.89,
+      "taux_criminalite": 0.00388,
       "gagnant": "LE PEN"
     },
     {
       "departement": "21",
-      "taux_criminalite": 4.16,
+      "taux_criminalite": 0.0011,
       "gagnant": "MACRON"
     },
     {
       "departement": "22",
-      "taux_criminalite": 3.46,
+      "taux_criminalite": 0.00081,
       "gagnant": "MACRON"
     },
     {
       "departement": "23",
-      "taux_criminalite": 3.05,
+      "taux_criminalite": 0.00164,
       "gagnant": "MACRON"
     },
     {
       "departement": "24",
-      "taux_criminalite": 3.4,
+      "taux_criminalite": 0.00118,
       "gagnant": "M\u00c9LENCHON"
     },
     {
       "departement": "25",
-      "taux_criminalite": 4.54,
+      "taux_criminalite": 0.00344,
       "gagnant": "LE PEN"
     },
     {
       "departement": "26",
-      "taux_criminalite": 5.01,
+      "taux_criminalite": 0.00172,
       "gagnant": "LE PEN"
     },
     {
       "departement": "27",
-      "taux_criminalite": 3.9,
+      "taux_criminalite": 0.00098,
       "gagnant": "LE PEN"
     },
     {
       "departement": "28",
-      "taux_criminalite": 3.8,
+      "taux_criminalite": 0.00135,
       "gagnant": "LE PEN"
     },
     {
       "departement": "29",
-      "taux_criminalite": 3.83,
+      "taux_criminalite": 0.00054,
       "gagnant": "MACRON"
     },
     {
       "departement": "30",
-      "taux_criminalite": 5.16,
+      "taux_criminalite": 0.00132,
       "gagnant": "LE PEN"
     },
     {
       "departement": "31",
-      "taux_criminalite": 6.57,
+      "taux_criminalite": 0.00116,
       "gagnant": "MACRON"
     },
     {
       "departement": "32",
-      "taux_criminalite": 3.35,
+      "taux_criminalite": 0.00051,
       "gagnant": "MACRON"
     },
     {
       "departement": "33",
-      "taux_criminalite": 5.65,
+      "taux_criminalite": 0.00106,
       "gagnant": "MACRON"
     },
     {
       "departement": "34",
-      "taux_criminalite": 6.52,
+      "taux_criminalite": 0.00103,
       "gagnant": "LE PEN"
     },
     {
       "departement": "35",
-      "taux_criminalite": 3.92,
+      "taux_criminalite": 0.00092,
       "gagnant": "MACRON"
     },
     {
       "departement": "36",
-      "taux_criminalite": 3.44,
+      "taux_criminalite": 0.00044,
       "gagnant": "LE PEN"
     },
     {
       "departement": "37",
-      "taux_criminalite": 4.51,
+      "taux_criminalite": 0.00145,
       "gagnant": "MACRON"
     },
     {
       "departement": "38",
-      "taux_criminalite": 5.58,
+      "taux_criminalite": 0.00125,
       "gagnant": "MACRON"
     },
     {
       "departement": "39",
-      "taux_criminalite": 3.52,
+      "taux_criminalite": 0.0,
       "gagnant": "LE PEN"
     },
     {
       "departement": "40",
-      "taux_criminalite": 3.82,
+      "taux_criminalite": 0.00048,
       "gagnant": "MACRON"
     },
     {
       "departement": "41",
-      "taux_criminalite": 4.14,
+      "taux_criminalite": 0.00029,
       "gagnant": "LE PEN"
     },
     {
       "departement": "42",
-      "taux_criminalite": 4.77,
+      "taux_criminalite": 0.00064,
       "gagnant": "LE PEN"
     },
     {
       "departement": "43",
-      "taux_criminalite": 3.11,
+      "taux_criminalite": 0.00085,
       "gagnant": "MACRON"
     },
     {
       "departement": "44",
-      "taux_criminalite": 5.88,
+      "taux_criminalite": 0.0007,
       "gagnant": "MACRON"
     },
     {
       "departement": "45",
-      "taux_criminalite": 4.75,
+      "taux_criminalite": 0.00087,
       "gagnant": "LE PEN"
     },
     {
       "departement": "46",
-      "taux_criminalite": 3.26,
+      "taux_criminalite": 0.00111,
       "gagnant": "MACRON"
     },
     {
       "departement": "47",
-      "taux_criminalite": 4.02,
+      "taux_criminalite": 0.00147,
       "gagnant": "LE PEN"
     },
     {
       "departement": "48",
-      "taux_criminalite": 2.63,
+      "taux_criminalite": 0.0,
       "gagnant": "FILLON"
     },
     {
       "departement": "49",
-      "taux_criminalite": 3.76,
+      "taux_criminalite": 0.00108,
       "gagnant": "MACRON"
     },
     {
       "departement": "50",
-      "taux_criminalite": 3.03,
+      "taux_criminalite": 0.00078,
       "gagnant": "MACRON"
     },
     {
       "departement": "51",
-      "taux_criminalite": 5.24,
+      "taux_criminalite": 0.00103,
       "gagnant": "LE PEN"
     },
     {
       "departement": "52",
-      "taux_criminalite": 4.07,
+      "taux_criminalite": 0.0,
       "gagnant": "LE PEN"
     },
     {
       "departement": "53",
-      "taux_criminalite": 3.16,
+      "taux_criminalite": 0.00063,
       "gagnant": "FILLON"
     },
     {
       "departement": "54",
-      "taux_criminalite": 4.69,
+      "taux_criminalite": 0.00054,
       "gagnant": "LE PEN"
     },
     {
       "departement": "55",
-      "taux_criminalite": 3.69,
+      "taux_criminalite": 0.00052,
       "gagnant": "LE PEN"
     },
     {
       "departement": "56",
-      "taux_criminalite": 3.63,
+      "taux_criminalite": 0.00078,
       "gagnant": "MACRON"
     },
     {
       "departement": "57",
-      "taux_criminalite": 4.05,
+      "taux_criminalite": 0.00085,
       "gagnant": "LE PEN"
     },
     {
       "departement": "58",
-      "taux_criminalite": 4.17,
+      "taux_criminalite": 0.00047,
       "gagnant": "LE PEN"
     },
     {
       "departement": "59",
-      "taux_criminalite": 6.06,
+      "taux_criminalite": 0.00121,
       "gagnant": "LE PEN"
     },
     {
       "departement": "60",
-      "taux_criminalite": 4.91,
+      "taux_criminalite": 0.00107,
       "gagnant": "LE PEN"
     },
     {
       "departement": "61",
-      "taux_criminalite": 3.78,
+      "taux_criminalite": 0.00034,
       "gagnant": "FILLON"
     },
     {
       "departement": "62",
-      "taux_criminalite": 4.72,
+      "taux_criminalite": 0.00107,
       "gagnant": "LE PEN"
     },
     {
       "departement": "63",
-      "taux_criminalite": 4.49,
+      "taux_criminalite": 0.0009,
       "gagnant": "MACRON"
     },
     {
       "departement": "64",
-      "taux_criminalite": 3.9,
+      "taux_criminalite": 0.00072,
       "gagnant": "MACRON"
     },
     {
       "departement": "65",
-      "taux_criminalite": 4.21,
+      "taux_criminalite": 0.00043,
       "gagnant": "MACRON"
     },
     {
       "departement": "66",
-      "taux_criminalite": 5.89,
+      "taux_criminalite": 0.00145,
       "gagnant": "LE PEN"
     },
     {
       "departement": "67",
-      "taux_criminalite": 4.24,
+      "taux_criminalite": 0.00079,
       "gagnant": "LE PEN"
     },
     {
       "departement": "68",
-      "taux_criminalite": 3.86,
+      "taux_criminalite": 0.00051,
       "gagnant": "LE PEN"
     },
     {
       "departement": "69",
-      "taux_criminalite": 7.47,
+      "taux_criminalite": 0.00144,
       "gagnant": "MACRON"
     },
     {
       "departement": "70",
-      "taux_criminalite": 3.31,
+      "taux_criminalite": 0.00206,
       "gagnant": "LE PEN"
     },
     {
       "departement": "71",
-      "taux_criminalite": 3.54,
+      "taux_criminalite": 0.00018,
       "gagnant": "LE PEN"
     },
     {
       "departement": "72",
-      "taux_criminalite": 4.19,
+      "taux_criminalite": 0.00069,
       "gagnant": "FILLON"
     },
     {
       "departement": "73",
-      "taux_criminalite": 4.88,
+      "taux_criminalite": 0.0009,
       "gagnant": "MACRON"
     },
     {
       "departement": "74",
-      "taux_criminalite": 4.47,
+      "taux_criminalite": 0.00157,
       "gagnant": "FILLON"
     },
     {
       "departement": "75",
-      "taux_criminalite": 12.67,
+      "taux_criminalite": 0.00186,
       "gagnant": "MACRON"
     },
     {
       "departement": "76",
-      "taux_criminalite": 4.84,
+      "taux_criminalite": 0.0011,
       "gagnant": "LE PEN"
     },
     {
       "departement": "77",
-      "taux_criminalite": 5.83,
+      "taux_criminalite": 0.00092,
       "gagnant": "MACRON"
     },
     {
       "departement": "78",
-      "taux_criminalite": 5.57,
+      "taux_criminalite": 0.00123,
       "gagnant": "MACRON"
     },
     {
       "departement": "79",
-      "taux_criminalite": 3.38,
+      "taux_criminalite": 0.00104,
       "gagnant": "MACRON"
     },
     {
       "departement": "80",
-      "taux_criminalite": 4.72,
+      "taux_criminalite": 0.00155,
       "gagnant": "LE PEN"
     },
     {
       "departement": "81",
-      "taux_criminalite": 3.73,
+      "taux_criminalite": 0.0005,
       "gagnant": "LE PEN"
     },
     {
       "departement": "82",
-      "taux_criminalite": 4.36,
+      "taux_criminalite": 0.00114,
       "gagnant": "LE PEN"
     },
     {
       "departement": "83",
-      "taux_criminalite": 5.82,
+      "taux_criminalite": 0.00205,
       "gagnant": "LE PEN"
     },
     {
       "departement": "84",
-      "taux_criminalite": 5.44,
+      "taux_criminalite": 0.0021,
       "gagnant": "LE PEN"
     },
     {
       "departement": "85",
-      "taux_criminalite": 3.29,
+      "taux_criminalite": 0.00014,
       "gagnant": "MACRON"
     },
     {
       "departement": "86",
-      "taux_criminalite": 4.13,
+      "taux_criminalite": 0.00089,
       "gagnant": "MACRON"
     },
     {
       "departement": "87",
-      "taux_criminalite": 3.57,
+      "taux_criminalite": 0.0,
       "gagnant": "MACRON"
     },
     {
       "departement": "88",
-      "taux_criminalite": 3.68,
+      "taux_criminalite": 0.00026,
       "gagnant": "LE PEN"
     },
     {
       "departement": "89",
-      "taux_criminalite": 4.84,
+      "taux_criminalite": 0.00115,
       "gagnant": "LE PEN"
     },
     {
       "departement": "90",
-      "taux_criminalite": 2.94,
+      "taux_criminalite": 0.0,
       "gagnant": "LE PEN"
     },
     {
       "departement": "91",
-      "taux_criminalite": 5.26,
+      "taux_criminalite": 0.00046,
       "gagnant": "MACRON"
     },
     {
       "departement": "92",
-      "taux_criminalite": 6.68,
+      "taux_criminalite": 0.00055,
       "gagnant": "MACRON"
     },
     {
       "departement": "93",
-      "taux_criminalite": 8.82,
+      "taux_criminalite": 0.00135,
       "gagnant": "M\u00c9LENCHON"
     },
     {
       "departement": "94",
-      "taux_criminalite": 6.54,
+      "taux_criminalite": 0.00086,
       "gagnant": "MACRON"
     },
     {
       "departement": "95",
-      "taux_criminalite": 6.69,
+      "taux_criminalite": 0.00056,
       "gagnant": "MACRON"
     }
   ]
